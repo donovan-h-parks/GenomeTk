42917318d3ce146f595625b312d4abdfb506725a
dparks1134 <donovan.parks@gmail.com>
2020-03-14 01:18:46 +1000

    Updates to setup.py.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 def version():
     setupDir = os.path.dirname(os.path.realpath(__file__))
     versionFile = open(os.path.join(setupDir, 'genometk', 'VERSION'))
-    return versionFile.read().strip()
+    return versionFile.readline().strip()
 
 setup(
     name='GenomeTk',
@@ -15,12 +15,11 @@ setup(
     author_email='donovan.parks@gmail.com',
     packages=['genometk'],
     scripts=['bin/genometk'],
-    package_data={'genometk' : ['VERSION', 'data_files/hmms/*']},
+    package_data={'genometk' : ['VERSION', 'data_files/barrnap/*']},
     url='http://pypi.python.org/pypi/genometk/',
     license='GPL3',
     description='A toolbox for working with genomes.',
-    long_description=open('README.md').read(),
     install_requires=[
         "numpy >= 1.8.0",
-        "biolib >= 0.0.11"],
+        "biolib >= 0.1.0"],
 )
